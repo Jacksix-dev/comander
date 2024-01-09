fa5b610b3cf850188bdd4bd3e936d51c92c54172
Lorenzo Castano <castanolorenzo776@gmail.com>
2024-01-09 17:19:22 -0300

    Done table controller, edit view, index view, show view

--- app/models/table.rb
+++ app/models/table.rb
@@ -4,4 +4,20 @@ class Table < ApplicationRecord
   belongs_to :user
   validates :number, presence: true
   validates :status, inclusion: { in: %w(empty reserved available checkout)}
+  validates :number, uniqueness: true
+  before_validation :set_default_status
+  before_validation :set_default_customer
+
+  def set_default_status
+    if self.status == "" || self.status == nil
+      self.status = "empty"
+    end
+  end
+
+  def set_default_customer
+    if self.customer_number == "" || self.customer_number == nil
+      self.customer_number = 0
+    end
+  end
+
 end
