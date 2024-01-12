4b44ecee5c6ac079431297346addd789d8bcc0da
Facundo Gasco <facundogasco@gmail.com>
2024-01-12 12:27:21 -0300

    resolution stimulus problem

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,7 @@
-# Pin npm packages by running ./bin/importmap
-
-pin "application"
+pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
-pin "tailwindcss-stimulus-components" # @4.0.4
